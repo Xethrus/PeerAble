39e84c55efc5da2e538bb194ea268ce70b167b99
xethrus <xethrus@gmail.com>
2023-03-22 23:00:30 -0700

    more weak_ptr

--- backend/src/server_basis/routing_server.cpp
+++ backend/src/server_basis/routing_server.cpp
@@ -16,12 +16,14 @@
 #include <map>
 #include <mutex>
 #include <set>
+#include <functional>
 #include <vector>
 
 typedef websocketpp::server<websocketpp::config::asio> server;
 
+ 
 std::mutex connections_mutex;
-std::map<int, websocketpp::connection_hdl>, std::owner_less<websocketpp::connection_hdl>> connections;
+//std::map<int, websocketpp::connection_hdl, std::owner_less<websocketpp::connection_hdl>> connections;
 
 class routing_server {
 public:
@@ -68,49 +70,49 @@ public:
 };
 
 
-
-//making alias 
-using tcp = boost::asio::ip::tcp;
-//adding namespace for easy ref
-namespace http = boost::beast::http;
-
-//takes tcp socket and a path string- we need to defined aserve file function because the http server needs to send static frontend files to clients
-void serve_file(tcp::socket &socket, const std::string &path) {
-  //Open the file to be served to the clients and determine its size, so that we can set the content-length header in the http response
-    http::file_body::value_type file;
-    file.open(path.c_str(), boost::beast::file_mode::read);
-    auto const size = file.size();
-//creates the ok reponse object for success use
-    http::response<http::file_body> res{
-        http::status::ok, 11};
-    //these set the server and content-type headers, they also assign the file object to the response body to send the file's contents to the client
-    res.set(http::field::server, "WebSocket++ Server");
-    res.set(http::field::content_type, "text/html");
-    res.content_length(size);
-    res.body() = std::move(file);
-    //this writes the response to the tcp socket passed in above
-    http::write(socket, res);
-}
-
-void start_http_server() {
-  //create I/0 context and tcp acceptor that listens on port 8080 with ipv4. Context and acceptor are needed to manage and accept incoming client connections
-  boost::asio::io_context ioc{1};
-  tcp::acceptor acceptor{ioc, {tcp::v4(), 80}};
-//this is an infinite loop so we can keep accepting incoming connections
-  for (;;) {
-    //new tcp socket using the i/o context
-    tcp::socket socket{ioc};
-    acceptor.accept(socket);
-
-    boost::beast:flat_buffer buffer;
-    http::request<http::string_body> request;
-    http::read<socket, buffer, request);
-
-    if (req.target() == "/") {
-        serve_file(socket, "frontend/index.html");
-    } 
-  }
-}
+//
+////making alias 
+//using tcp = boost::asio::ip::tcp;
+////adding namespace for easy ref
+//namespace http = boost::beast::http;
+//
+////takes tcp socket and a path string- we need to defined aserve file function because the http server needs to send static frontend files to clients
+//void serve_file(tcp::socket &socket, const std::string &path) {
+//  //Open the file to be served to the clients and determine its size, so that we can set the content-length header in the http response
+//    http::file_body::value_type file;
+//    file.open(path.c_str(), boost::beast::file_mode::read);
+//    auto const size = file.size();
+////creates the ok reponse object for success use
+//    http::response<http::file_body> res{
+//        http::status::ok, 11};
+//    //these set the server and content-type headers, they also assign the file object to the response body to send the file's contents to the client
+//    res.set(http::field::server, "WebSocket++ Server");
+//    res.set(http::field::content_type, "text/html");
+//    res.content_length(size);
+//    res.body() = std::move(file);
+//    //this writes the response to the tcp socket passed in above
+//    http::write(socket, res);
+//}
+//
+//void start_http_server() {
+//  //create I/0 context and tcp acceptor that listens on port 8080 with ipv4. Context and acceptor are needed to manage and accept incoming client connections
+//  boost::asio::io_context ioc{1};
+//  tcp::acceptor acceptor{ioc, {tcp::v4(), 80}};
+////this is an infinite loop so we can keep accepting incoming connections
+//  for (;;) {
+//    //new tcp socket using the i/o context
+//    tcp::socket socket{ioc};
+//    acceptor.accept(socket);
+//
+//    boost::beast:flat_buffer buffer;
+//    http::request<http::string_body> request;
+//    http::read<socket, buffer, request);
+//
+//    if (req.target() == "/") {
+//        serve_file(socket, "frontend/index.html");
+//    } 
+//  }
+//}
 
 void start_websocket_server() {
   routing_server route_srv;
@@ -120,9 +122,9 @@ void start_websocket_server() {
   ws_server.init_asio();
   
   //setup handlers
-  ws_server.set_open_handler(bind(&routing_server::on_open, &route_srv, ::_1));
-  ws_server.set_close_handler(bind(&routing_server::on_close, &route_srv, ::_1));
-  ws_server.set_message_handler(bind(&routing_server::on_message, &route_srv, ::_1, ::_2));
+  ws_server.set_open_handler(bind(&routing_server::on_open, &route_srv, std::placeholders::_1));
+  ws_server.set_close_handler(bind(&routing_server::on_close, &route_srv, std::placeholders::_1));
+  ws_server.set_message_handler(bind(&routing_server::on_message, &route_srv, std::placeholders::_1, std::placeholders::_2));
 
   //listen on port
   uint16_t port = 8000;
@@ -137,10 +139,11 @@ void start_websocket_server() {
 
 
 int main() {
-  std::thread http_server_thread(start_http_server);
+  //std::thread http_server_thread(start_http_server);
+
   std::thread websocket_server_thread(start_websocket_server);
 
-  http_server_thread.join();
+  //http_server_thread.join();
   websocket_server_thread.join();
 
   return 0;
