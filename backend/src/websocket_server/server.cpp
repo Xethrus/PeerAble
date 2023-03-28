c5fb67e71bc3457ea6549fcefda8670ee72b26b2
xethrus <xethrus@gmail.com>
2023-03-27 22:17:38 -0700

    it breaks by passing wrong args

--- backend/src/websocket_server/server.cpp
+++ backend/src/websocket_server/server.cpp
@@ -1,25 +1,41 @@
 #include "mongoose.h"
+#include <iostream>
+#include <string>
 
-
+static const char *s_listen_on = "ws://107.131.124.5:8000";
+static const char *s_web_root = ".";
 
 static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
-  if (ev == MG_EV_HTTP_MSG) {
-    struct mg_http_message *hm = (struct mg_http_message *) ev_data;
-    if (mg_http_match_uri(hm, "/api/hello")) {              // On /api/hello requests,
-      mg_http_reply(c, 200, "", "{%Q:%d}\n", "status", 1);  // Send dynamic JSON response
-    } else {                                                // For all other URIs,
-      struct mg_http_serve_opts opts = {.root_dir = "."};   // Serve files
-      mg_http_serve_dir(c, hm, &opts);                      // From root_dir
+  if (ev == MG_EV_OPEN) {
+    // c->is_hexdumping = 1;
+  } else if (ev == MG_EV_HTTP_MSG) {
+    mg_http_message *hm = (mg_http_message *) ev_data;
+    if (mg_http_match_uri(hm, "/websocket")) {
+      // Upgrade to websocket. From now on, a connection is a full-duplex
+      // Websocket connection, which will receive MG_EV_WS_MSG events.
+      mg_ws_upgrade(c, hm, NULL);
+    } else if (mg_http_match_uri(hm, "/rest")) {
+      // Serve REST response
+      mg_http_reply(c, 200, "", "{\"result\": %d}\n", 123);
+    } else {
+      // Serve static files
+      mg_http_serve_opts opts = {.root_dir = s_web_root};
+      mg_http_serve_dir(c, ev_data, &opts);
     }
+  } else if (ev == MG_EV_WS_MSG) {
+    // Got websocket frame. Received data is wm->data. Echo it back!
+    struct mg_ws_message *wm = (struct mg_ws_message *) ev_data;
+    mg_ws_send(c, wm->data.ptr, wm->data.len, WEBSOCKET_OP_TEXT);
   }
+  (void) fn_data;
 }
 
-int main() {
-  struct mg_mgr mgr; 
-  mg_mgr_init(&mgr); // Init manager
-  mg_http_listen(&mgr, "http://107.131.124.5:8000", fn, &mgr);// Setup listener
-  for (;;) mg_mgr_poll(&mgr, 1000); // Infinite event loop
-  
-
+int main(void) {
+  struct mg_mgr mgr;  // Event manager
+  mg_mgr_init(&mgr);  // Initialise event manager
+  printf("Starting WS listener on %s/websocket\n", s_listen_on);
+  mg_http_listen(&mgr, s_listen_on, fn, NULL);  // Create HTTP listener
+  for (;;) mg_mgr_poll(&mgr, 1000);             // Infinite event loop
+  mg_mgr_free(&mgr);
   return 0;
 }
